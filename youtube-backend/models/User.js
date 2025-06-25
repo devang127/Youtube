1c97dfdbd3eac64149d7cc65df499916738c83d7
Devang Sawant <devang.ideamagix@gmail.com>
2025-06-25 15:23:27 +0530

    MF

--- youtube-backend/models/User.js
+++ youtube-backend/models/User.js
@@ -5,13 +5,13 @@ const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     channelName:{type:String, required:true},
     email:{type:String, required:true},
-    phone:{type:String, required:true},
+    phone:{type:Number, required:true},
     password: {type:String, required:true},
     logoUrl: {type:String, required:true},
     logoId: {type:String, required:true},
     subscribers: {type:Number, required:true},
-    subscribedChannels: [{type:mongoose.Schema.Types.ObjectId, ref: 'User'}]
-
+    subscribedChannels: [{type:mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    Timestamp: {type:Date, default:Date.now},
 })
 
 module.exports = mongoose.model('User', userSchema)
